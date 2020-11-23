1e9cbeb2f5dfd2442a80f7ccb8691bba75f7b0d1
GalaxyGamingBoy <xrteach@gmail.com>
2020-11-23 15:59:21 +0200

    Added Source Code -Semi-Final-
    Need to add Start Program Explenation

--- main.c
+++ main.c
@@ -1,29 +1,66 @@
 #include <stdio.h>
-int main() {
-    char operator;
-    double first, second;
-    printf("Enter an operator (+, -, *,): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &first, &second);
 
-    switch (operator) {
-    case '+':
-        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
-        break;
-    case '-':
-        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-        break;
-    case '*':
-        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
-        break;
-    case '/':
-        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-        break;
-        // operator doesn't match any case constant
-    default:
-        printf("Error! operator is not correct");
+double askUserNum(int count)
+{
+    double num;
+    printf("Enter your %d number you wish to use: ", count);
+    scanf("%lf", &num);
+    return num;
+}
+double calculate(char op)
+{
+    double sum, num_1, num_2;
+    switch(op){
+        case '+':
+            num_1 = askUserNum(1);
+            num_2 = askUserNum(2);
+            sum = num_1 + num_2;
+            return sum;
+            break;
+        case '-':
+            num_1 = askUserNum(1);
+            num_2 = askUserNum(2);
+            sum = num_1 - num_2;
+            return sum;
+            break;
+        case '*':
+            num_1 = askUserNum(1);
+            num_2 = askUserNum(2);
+            sum = num_1 * num_2;
+            return sum;
+            break;
+        case '/':
+            num_1 = askUserNum(1);
+            num_2 = askUserNum(2);
+            sum = num_1 / num_2;
+            return sum;
+            break;
+        case 'X':
+            sum = 01011000;
+            return sum;
+            break;
+        }
+}
+
+void mainLoop()
+{
+    char op;
+    double sum;
+    int i = 0;
+    while(i != 1)
+    {
+        printf("Enter the operator you wish to use: ");
+        scanf(" %s", &op);
+        if(sum == 01011000)
+        {
+            i = 1;
+        }else{
+            sum = calculate(op);
+            printf("%f\n", sum);
+        }
     }
-
+}
+int main() {
+    mainLoop();
     return 0;
 }
